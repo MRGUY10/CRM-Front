da85a1b0cf31aa64f6c3b8ee303d4d2e8fd725e1
Frederic Onana <onanafrederic31@gmail.com>
2025-04-27 15:58:27 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,55 @@
-import { Component,OnInit } from '@angular/core';
-import { RouterOutlet,Router,NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { filter } from 'rxjs/operators';
 import { AuthService } from './services/auth/auth.service';
+import { MultiStepFormComponent } from './lib/multi-step-form.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent,NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, HeaderComponent, MultiStepFormComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+  title = 'CRM';
+  showLayout = true;
 
-  currentComponent: string = "";
-
-  constructor(private router: Router,private authService: AuthService){}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     // Clear token on app reload
-    //  this.authService.clearTokenOnReload();
+    this.authService.clearTokenOnReload();
 
-    // Clear token on navigation to login page
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      const currentRoute = this.router.routerState.root.firstChild;
+    // Update layout visibility based on route data
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentRoute = this.getChild(this.route);
 
-      if (currentRoute && currentRoute.component) {
-        this.currentComponent = currentRoute.component?.name || ' ';
-      }
+        currentRoute.data.subscribe(data => {
+          this.showLayout = data['layout'] !== false;
+        });
 
-      if (this.router.url === '/login') {
-        this.authService.clearTokenOnReload();
-      }
-    });
+        if (this.router.url === '/login') {
+          this.authService.clearTokenOnReload();
+        }
+      });
   }
-  title = 'CRM';
+
+  getChild(route: ActivatedRoute): ActivatedRoute {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
 }
